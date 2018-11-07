3d48d5f8780f09a4df0f813873fc29fdcb9a5553
Katherine Riley <unoscarto@hotmail.com>
2018-11-07 11:47:32 -0800

    moved button to sit next to search box

--- src/ResultsList.js
+++ src/ResultsList.js
@@ -27,13 +27,14 @@ class ResultsList extends Component {
               onChange={(event) => this.props.userQuery(event.target.value)}
               />
           </div>
+          <button
+          className = 'toggle-results-list'>
+          </button>
         </div>
         <header className = 'results-header-body'>
           <div className = 'results-title'>
             <h2>Search Results</h2>
           </div>
-          <button className = 'close-results-list'>
-          </button>
         </header>
         <ol className = "search-results-body">
 
